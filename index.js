da1b6d284167feb1b9ade04ac0f0bab8c5c9e025
Collins Kimani Karanja <kimcolinss@gmail.com>
2022-08-25 14:12:38 +0300

    COmpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(game="roller-skate"){
+    return(`This Saturday, I want to ${game}!`)
+}
+function mondayWork(activity="go to the office"){
+    return(`This Monday, I will ${activity}.`)
+}
+function wrapAdjective(offer="*"){
+    return function(word  = "Special"){
+    return `You are ${offer}${word}${offer}!`
+    }
+}
